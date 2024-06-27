0d41f8421588e81e535fc74c5e26855eee32aeb5
iczky <fiqra22@gmail.com>
2024-06-27 14:29:31 +0700

    feat: initialize the server for event management server 1. init folder for feature event 2. init exceptions and response 3. create docker-compose to initialize postgres 4. add env file

---
+++ src/test/java/com/eventhive/eventHive/EventHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventhive.eventHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
