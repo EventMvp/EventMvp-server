6bf9f01f87fa4ae7cdc7632fe5d2d55029b2627a
iczky <fiqra22@gmail.com>
2024-07-09 15:30:36 +0700

    fix: change dotenv usage to @Value 1. Add cloudinary value to app properties 2. add those value to cloudinary config using @Value

--- src/main/java/com/eventhive/eventHive/config/CloudinaryConfig.java
+++ src/main/java/com/eventhive/eventHive/config/CloudinaryConfig.java
@@ -3,18 +3,27 @@ package com.eventhive.eventHive.config;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
+    @Value("${CLOUDINARY_NAME}")
+    private String cloudinaryName;
+
+    @Value("${CLOUDINARY_API_KEY}")
+    private String cloudinaryApiKey;
+
+    @Value("${CLOUDINARY_API_SECRET}")
+    private String cloudinaryApiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
-        Dotenv dotenv = Dotenv.load();
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", dotenv.get("CLOUDINARY_NAME"),
-                "api_key", dotenv.get("CLOUDINARY_API_KEY"),
-                "api_secret", dotenv.get("CLOUDINARY_API_SECRET")
+                "cloud_name", cloudinaryName,
+                "api_key", cloudinaryApiKey,
+                "api_secret", cloudinaryApiSecret
         ));
     }
 }
